7602c8aa9667c35c3c6806a1910e822137a9b61e
Lucas Oliveira <lucasprogjr@gmail.com>
2025-03-17 19:34:15 -0300

    NEW: Beecrowd - 1001 - JS

---
+++ BeeCrowd/1001/extremamente-basico.js
@@ -0,0 +1,24 @@
+import fs from "fs";
+import path from "path";
+
+const filePath = path.resolve(
+  path.dirname(new URL(import.meta.url).pathname),
+  "entrada.txt"
+);
+const input = fs.readFileSync(filePath, "utf8");
+const lines = input.split("\n");
+
+/**
+ * Escreva a sua solução aqui abaixo
+ */
+const num1 = Number(lines[0]);
+const num2 = Number(lines[1]);
+
+console.log("X = " + (num1 + num2));
+
+// const num1 = Number(lines[0]);
+// const num2 = Number(lines[1]);
+// const soma = num1 + num2;
+// console.log(soma);
+
+// console.log(Number(lines[0]) + Number(lines[1]));
