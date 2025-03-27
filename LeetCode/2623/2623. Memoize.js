8dfa28a54695b88d682694999f47eefa16734fc8
Lucas Oliveira <lucasprogjr@gmail.com>
2025-03-27 11:06:21 -0300

    NEW: 2623 - Leetcode - JS

---
+++ LeetCode/2623/2623. Memoize.js
@@ -0,0 +1,37 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    const key = JSON.stringify(args);
+    if (key in cache) {
+      return cache[key];
+    }
+    const result = fn.apply(this, args);
+    cache[key] = result;
+
+    return result;
+  };
+}
+
+/**
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1
+ */
+
+let callCount = 0;
+const memoizedSumFn = memoize(function sum(a, b) {
+  callCount += 1;
+  return a + b;
+});
+console.log(memoizedSumFn(2, 3)); // 5
+console.log(memoizedSumFn(2, 3)); // 5
+console.log(callCount); // 1
